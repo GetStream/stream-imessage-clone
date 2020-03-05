e8ea724f5d7155a376f2153603d1a5fc1cc26d27
Bahadir Oncel <bahadir.onc@gmail.com>
2020-03-05 16:16:45 +0100

    Starter branch cleanup

--- iMessageClone/AppDelegate.swift
+++ iMessageClone/AppDelegate.swift
@@ -11,12 +11,6 @@ import StreamChatClient
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Client.config = .init(apiKey: "b67pax5b2wdq", logOptions: .info)
-        Client.shared.set(user: User(id: "polished-poetry-5",
-                                     name: "Polished poetry"),
-                          token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoicG9saXNoZWQtcG9ldHJ5LTUifQ.o8SWzSlb68EntudwjVul1rUCYGpla-CimXNKxj3wKOc")
-        return true
-    }
+
 }
 
